898efd01230d9b22e02f2c947d730f323a0c55c5
Soroush Kazemi <kazemi.soroush@gmail.com>
2026-02-22 05:47:59 +0000

    Use requirements.txt for devcontainer and CI dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,7 @@ git config --global user.email 'kazemi.soroush@gmail.com'
 git config --global user.name 'Soroush Kazemi'
 
 echo "Installing Python dependencies..."
-python3 -m pip install --break-system-packages pytest flake8
+python3 -m pip install --break-system-packages -r requirements.txt
 
 echo "Running tests..."
 python3 -m pytest src/ -v
